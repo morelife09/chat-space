ca031a17500472dd9f30607e7931e62c6b4d0f3c
morelife09 <junichi1995930@gmail.com>
2020-06-14 20:58:46 +0900

    start1

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,27 +2,26 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       var html =
-        `
-          <div class="main__chats__message">
-            <div class="main__chats__message__poster">
-              ${message.user_name}
+        `<div class="messages" data-message-id=${message.id}>
+            <div class="main__chats__message">
+              <div class="main__chats__message__poster">
+                ${message.user_name}
+              </div>
+              <div class="main__chats__message__date">
+                ${message.created_at}
+              </div>
             </div>
-            <div class="main__chats__message__date">
-              ${message.created_at}
+            <div class="main__chats__comment">
+              <p class="main__chats__comment__content">
+                ${message.content}
+              </p>
             </div>
-          </div>
-          <div class="main__chats__comment">
-            <p class="main__chats__comment__content">
-              ${message.content}
-            </p>
-          </div>
-          <img src=${message.image} >
+            <img src=${message.image} >
         </div>`
       return html;
     } else {
       var html =
-        `
-          <div class="main__chats__message">
+        `<div class="main__chats__message" data-message-id=${message.id}>
             <div class="main__chats__message__poster">
               ${message.user_name}
             </div>
@@ -62,4 +61,36 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     });
   });
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      //ルーティングで設定した通りのURLを指定
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        //追加するHTMLの入れ物を作る
+        var insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
